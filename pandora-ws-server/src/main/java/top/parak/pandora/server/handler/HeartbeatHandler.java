48bcfc5fe3d391372f4fe46adf7ae399dbbb5de1
cantai <cantai@pinduoduo.com>
2023-02-23 23:08:22 +0800

    Pandora-WS-Server: HeartbeatHandler coding.

--- pandora-ws-server/src/main/java/top/parak/pandora/server/handler/HeartbeatHandler.java
+++ pandora-ws-server/src/main/java/top/parak/pandora/server/handler/HeartbeatHandler.java
@@ -2,8 +2,13 @@ package top.parak.pandora.server.handler;
 
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.timeout.IdleState;
+import io.netty.handler.timeout.IdleStateEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import sun.rmi.runtime.Log;
+
+import java.net.SocketAddress;
 
 /**
  * @author cantai
@@ -16,7 +21,19 @@ public class HeartbeatHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
         super.userEventTriggered(ctx, evt);
-        //
+        if (evt instanceof IdleStateEvent) {
+            IdleStateEvent event = (IdleStateEvent) evt;
+            SocketAddress clientAddr = ctx.channel().remoteAddress();
+            if (event.state() == IdleState.READER_IDLE) {
+                LOG.info("[userEventTriggered] {} -> read idle", clientAddr);
+            } else if (event.state() == IdleState.WRITER_IDLE) {
+                LOG.info("[userEventTriggered] {} -> write idle", clientAddr);
+            } else if (event.state() == IdleState.ALL_IDLE) {
+                LOG.info("[userEventTriggered] {} -> all idle", clientAddr);
+                ctx.channel().close();
+                LOG.info("|==================== [断开连接: {}] ====================|", clientAddr);
+            }
+        }
     }
 
 }
